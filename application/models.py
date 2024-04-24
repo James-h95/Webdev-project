611ab15b8644af37d4a5e11ad946d86812535983
James Hudibyo <hudibyojames@gmail.com>
2024-04-24 14:55:54 +0800

    Added things to model

--- application/models.py
+++ application/models.py
@@ -1,9 +1,19 @@
 from application import db
+
+class User(db.Model):
+    id = db.column(db.String(length=30),nullable=False,unique=True)
+    username =db.Column(db.String(length=30),nullable=False,unique=True)
+    email_address = db.column(db.String(length=50),nullable=False,unique=True)
+    password = db.Column(db.String(length=60), nullable=False)
+    balance = db.Column(db.Integer(),nullable=False,default=10)
+    items = db.relationship('Item',backref='owner',lazy=true)
+
 class Item(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(length=30),nullable=False,unique=True)
     price = db.Column(db.Integer(),nullable=False)
     rarity = db.Column(db.String(length=30),nullable=False)
+    owner = db.Column(db.Integer(),db.ForeignKey('user.id'))
     
     def __reprr__(self):
         return f'Item {self.name}'
