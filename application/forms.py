2c1a7b1d5f5697b73114f253517ef72eca2e96f8
carlymayyy <scottcarly37@gmail.com>
2024-05-08 15:31:30 +0800

    Finished create form to allow users to make games, ready for merging.

--- application/forms.py
+++ application/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import Length, EqualTo, DataRequired
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField, IntegerField
+from wtforms.validators import Length, EqualTo, DataRequired, NumberRange, Regexp
 
 # ISSUES
 # Field showing which country user belongs to? Some radio type button maybe, use this as info
@@ -17,6 +17,9 @@ class RegisterForm(FlaskForm):
     password2 = StringField('Confirm Password:',validators=[EqualTo('password1'), DataRequired()])
     submit = SubmitField('Sign Up')
     
-    
-
-    
+class CreateGameForm(FlaskForm):
+    category = SelectField('Category:',choices=[("media", "Media"), ("books", "Books"), ("history", "History"), ("sports", "Sports"), ("music", "Music"), ("games", "Games"), ("places", "Places"), ("food", "Food"), ("misc", "Misc.")], validators=[DataRequired()])
+    timeLimit = IntegerField('Time Limit (s):',validators=[NumberRange(min=30,max=120),DataRequired()])
+    phraseValidator = Regexp(regex=r'^[a-zA-Z\s,"\'\?!:;.]+$', message='Must contain only letters and the following punctuation: ''"".,?!:;')
+    phrase = StringField('Word/phrase:',validators=[Length(min=3,max=250), phraseValidator,DataRequired()])
+    caption = StringField('Caption (optional):',validators=[Length(max=200)])
