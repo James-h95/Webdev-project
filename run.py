cfaa64dfc7ae04f5955766ceaa357acefcccf883
Joshua Mance <75736470+JoshMance@users.noreply.github.com>
2024-05-15 20:12:14 +0800

    Fixing bugs from integrating chat with feed

--- run.py
+++ run.py
@@ -1,5 +1,13 @@
 from application import app
 
+# from application.models import db,Item,User,Game
+# with app.app_context():
+#     db.drop_all() 
+#     db.create_all()
+#     db.session.commit()
+
+
+
 #checks if run.py file execute directly
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
